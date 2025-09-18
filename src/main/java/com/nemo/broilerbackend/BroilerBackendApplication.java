9cd7bae17709a7a8a0c1721e6e19514ffaf35be0
Marvin Christian <marvin.christian@yatta.de>
2025-09-18 10:32:13 +0200

    initial commit

---
+++ src/main/java/com/nemo/broilerbackend/BroilerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nemo.broilerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroilerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BroilerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nemo/broilerbackend/BroilerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemo.broilerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroilerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
