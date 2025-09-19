4ab318087047af51c86deb13d2306ef11a1b1617
Marv1nCh <Marvi1999@gmx.de>
2025-09-19 15:13:42 +0200

    Changed repositories to be of identical format

--- src/main/java/com/nemo/broilerbackend/purchase/PurchaseRepository.java
+++ src/main/java/com/nemo/broilerbackend/purchase/PurchaseRepository.java
@@ -2,9 +2,6 @@ package com.nemo.broilerbackend.purchase;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+public interface PurchaseRepository extends JpaRepository<Purchase,Long> {
 
-public interface PurchaseRepository extends JpaRepository<Purchase,Integer> {
-
-    Optional<Purchase> getById(Long id);
 }
--- src/main/java/com/nemo/broilerbackend/user/UserRepository.java
+++ src/main/java/com/nemo/broilerbackend/user/UserRepository.java
@@ -2,8 +2,5 @@ package com.nemo.broilerbackend.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-    Optional<User> getById(Long id);
+public interface UserRepository extends JpaRepository<User,Long> {
 }
