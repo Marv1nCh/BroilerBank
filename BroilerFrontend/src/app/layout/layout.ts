72398fcde6037fb6cb6faec01c8ec3e3965af56c
Marv1nCh <Marvi1999@gmx.de>
2025-10-13 11:45:32 +0200

    Added a darkmode

--- BroilerFrontend/src/app/layout/layout.ts
+++ BroilerFrontend/src/app/layout/layout.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Sidebar } from './components/sidebar/sidebar';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-layout',
-  imports: [Sidebar, CommonModule, RouterOutlet],
+  imports: [Sidebar, CommonModule, RouterOutlet, MatButtonModule, MatIconModule],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
-export class Layout {
+export class Layout implements OnInit {
   isSidebarCollapsed = false;
+  isDarkMode = false;
+
+  ngOnInit(): void {
+    const isDeviceStandardDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    this.isDarkMode = isDeviceStandardDarkMode;
+    if (isDeviceStandardDarkMode) document.body.classList.toggle('dark-mode');
+  }
 
   onSidebarToggle() {
     this.isSidebarCollapsed = !this.isSidebarCollapsed;
+  }
+
+  onToggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-mode');
   }
 }
